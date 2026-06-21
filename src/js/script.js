ee40cc4d8a537a9be79631459e1e0071c4c9906d
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-06-20 21:03:04 -0300

    feat: adicionado sistema de imagem

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,25 @@ const nav = document.querySelector("#nav");
 menuToggle.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
+
+const images = document.querySelectorAll(".card img");
+const lightbox = document.querySelector("#lightbox");
+const lightboxImg = document.querySelector("#lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(image => {
+    image.addEventListener("click", () => {
+        lightbox.classList.add("active");
+        lightboxImg.src = image.src;
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+});
+
+lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+        lightbox.classList.remove("active");
+    }
+});
